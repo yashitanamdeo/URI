e194f9af70dd404bd55aa20884d9099de298ca6e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-01 22:57:37 +0530

    add average 2

---
+++ Beginner/7_average2.py
@@ -0,0 +1,12 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+weightA = 2
+weightB = 3
+weightC = 5
+weightSum = weightA + weightB + weightC
+
+Average = ((weightA*A) + (weightB*B) + (weightC*C)) / weightSum
+
+print("MEDIA =",format(Average,".1f"))
